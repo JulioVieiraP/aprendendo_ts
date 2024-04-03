4636573113ad7379b3aca0c61caefa3813d55eec
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-04-03 14:55:34 -0300

    Exercicio completo

---
+++ src/script.js
@@ -0,0 +1,9 @@
+"use strict";
+function Multiplicação(num1, num2) {
+    return num1 * num2;
+}
+function Nome(nome) {
+    return `Olá ${nome}`;
+}
+console.log(Nome("julio"));
+console.log(Multiplicação(5, 10));
---
+++ src/script.ts
@@ -0,0 +1,10 @@
+function Multiplicação(num1: number, num2: number) {
+    return num1 * num2
+}
+
+function Nome(nome:string) {
+    return `Olá ${nome}`
+}
+
+console.log(Nome("julio"))
+console.log(Multiplicação(5, 10))
